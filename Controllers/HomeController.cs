54a7204d5ce831c8493c76d7725f922c7846de01
abdelrahmanMohamed <abdo.mohamed.am.ao@gmail.com>
2018-10-13 09:22:10 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DropdownList.Models;
+
+namespace DropdownList.Controllers
+{
+    public class HomeController : Controller
+    {
+        dropdownEntities db = new dropdownEntities();
+        public ActionResult Index()
+        {
+            List<Table_Country> CountryList = db.Table_Country.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "CountryId", "CountryName");
+            return View();
+        }
+
+        public JsonResult GetStateList(int CountryId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Table_State> StateList = db.Table_State.Where(x => x.CountryId == CountryId).ToList();
+            return Json(StateList, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Models/CountryStateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DropdownList.Models
+{
+    public class CountryStateViewModel
+    {
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+    }
+}
